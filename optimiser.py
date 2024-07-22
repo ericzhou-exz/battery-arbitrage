5dfab35a49a7588f256d560f6920c72b8ee8b2cf
Eric <ericzhou.exz@gmail.com>
2024-07-22 17:37:49 +1000

    trying rolling window

--- optimiser.py
+++ optimiser.py
@@ -4,7 +4,7 @@ import pyomo
 import matplotlib
 
 # Step 1: Load the Data
-data = pd.read_csv('qld/concatenated_output.csv')
+data = pd.read_csv('nsw/concatenated_output.csv')
 prices = data['RRP'].values
 
 # Step 2: Define Battery Parameters
@@ -14,30 +14,39 @@ max_discharge_rate = 1  # Maximum discharge rate in MW
 efficiency = 0.9  # Round-trip efficiency
 soc = 0.5
 
-sell = 200
-buy = 50
+#sell = 200
+#buy = 50
 
-for sell in [100, 200, 500, 1000, 2000, 5000, 10000]:
-    for buy in [0, 10, 20, 50, 100, 200, 500, 1000, 2000]:
-        for capacity in [1, 2, 4, 8, 16]:
-            total_charge_cost = 0
-            total_discharge_revenue = 0
-            charge_status = []
+sell_arr = pd.Series(prices).rolling(1000).quantile(0.9, interpolation='linear')
+buy_arr =  pd.Series(prices).rolling(1000).quantile(0.1, interpolation='linear')
+#print(sell_arr)
 
-            for i, price in enumerate(prices):
-                if price > sell and soc > 0.1:
-                    total_discharge_revenue += price / 12
-                    soc -= (max_discharge_rate / capacity) / 12
-                    charge_status.append(1)
+#for sell in [100, 200, 500, 1000, 2000, 5000, 10000]:
+#    for buy in [0, 10, 20, 50, 100, 200, 500, 1000, 2000]:
+for capacity in [1, 2, 4, 8, 16]:
+    total_charge_cost = 0
+    total_discharge_revenue = 0
+    charge_status = []
 
-                elif price < buy and soc < 0.9:
-                    total_charge_cost += price / 12
-                    soc += (max_charge_rate / capacity) / 12
-                    charge_status.append(-1)
+    for i, price in enumerate(prices):
+        #sell = np.quantile(prices[i:i+100], 0.75)
+        #buy = np.quantile(prices[i:i+100], 0.25)
+        sell = sell_arr[i]
+        buy = buy_arr[i]
 
-                else:
-                    charge_status.append(0)
+        if price > sell and soc > 0.1:
+            total_discharge_revenue += price / 12
+            soc -= (max_discharge_rate / capacity) / 12
+            charge_status.append(1)
 
-            utilisation = np.count_nonzero(charge_status) / len(prices)
-            profit = total_discharge_revenue - total_charge_cost
-            print(profit, sell, buy, capacity, utilisation)
+        elif price < buy and soc < 0.9:
+            total_charge_cost += price / 12
+            soc += (max_charge_rate / capacity) / 12
+            charge_status.append(-1)
+
+        else:
+            charge_status.append(0)
+
+    utilisation = np.count_nonzero(charge_status) / len(prices)
+    profit = total_discharge_revenue - total_charge_cost
+    print(profit, sell, buy, capacity, utilisation)
